981ac3940cb5fbd4bc3f0a8137f06795ca82c9ae
Mezenin Roman <gydroperit@gmail.com>
2015-07-12 09:10:31 -0700

    ss

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,19 +1,25 @@
 class MainController < ApplicationController
   protect_from_forgery with: :null_session
-  def index
-    @Project = Project.all
-
-    respond_to do |format|
-
-  format.html # show.html.erb
-  format.json { render :json => @Project }
-
+ def index
  end
 
+   def todo
+    @Todos = Todo.all.where(project_id: params[:id] )
+    respond_to do |format|
+      format.html
+      format.json { render :json => @Todos}
+
+      end
   end
   def create
   	Todo.create(text: params[:form], project_id: params[:slct])
   	redirect_to :back
+  end
+  def project
+    @Project = Project.all
+    respond_to do |format|
+      format.json { render :json => @Project }
+    end
   end
   def update
   		puts params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
    #resources :main
    post '/main', to: 'main#create'
    put '/main/:id', to: 'main#update'
-   get 'todo/index', to: 'main#index'
+   get 'todo/index', to: 'main#todo'
+   get 'project/index', to: 'main#project'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
