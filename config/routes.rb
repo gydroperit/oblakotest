a66693c43e7b05efca790385b34a5c9a14d02362
Mezenin Roman <gydroperit@gmail.com>
2015-07-12 04:50:23 -0700

    ss

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
    #resources :main
    post '/main', to: 'main#create'
    put '/main/:id', to: 'main#update'
+   get 'todo/index', to: 'main#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
